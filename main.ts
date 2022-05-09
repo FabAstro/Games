03c39e2b7c16449988e20ca209c6ff5b4bcec5b5
FabAstro <105211458+FabAstro@users.noreply.github.com>
2022-05-09 13:32:34 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,5 @@
 input.onButtonPressed(Button.A, function () {
     if (IconGame == 0) {
-        IconGame = 1
-    } else {
-        IconGame = 0
-    }
-})
-input.onButtonPressed(Button.B, function () {
-    if (IconGame == 0) {
         basic.showLeds(`
             # # . . .
             # # # . .
@@ -19,6 +12,99 @@ input.onButtonPressed(Button.B, function () {
         basic.showString("d'autres jeux sont à venir dans les prochaines version")
     }
 })
+input.onGesture(Gesture.TiltRight, function () {
+    if (Ecran1Serpant == 1) {
+        basic.showLeds(`
+            . # # . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . #
+            `)
+        Ecran1Serpant = 2
+    }
+    if (Ecran1Serpant == 2) {
+        basic.showLeds(`
+            . . # # .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . #
+            `)
+        Ecran1Serpant = 3
+    }
+    if (Ecran1Serpant == 3) {
+        basic.showLeds(`
+            . . . # #
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . #
+            `)
+        Ecran1Serpant = 4
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (IconGame == 0) {
+        IconGame = 1
+    } else {
+        IconGame = 0
+    }
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    if (IconGame == 0) {
+        basic.showLeds(`
+            # # . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . #
+            `)
+        Ecran1Serpant = 1
+    }
+})
+input.onGesture(Gesture.LogoDown, function () {
+    if (Ecran1Serpant == 4) {
+        basic.showLeds(`
+            . . . . #
+            . . . . #
+            . . . . .
+            . . . . .
+            . . . . #
+            `)
+        Ecran1Serpant = 5
+    }
+    if (Ecran1Serpant == 5) {
+        basic.showLeds(`
+            . . . . .
+            . . . . #
+            . . . . #
+            . . . . .
+            . . . . #
+            `)
+        Ecran1Serpant = 6
+    }
+    if (Ecran1Serpant == 6) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . #
+            . . . . #
+            . . . . #
+            `)
+        Ecran1Serpant = 7
+    }
+    if (Ecran1Serpant == 8) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . # . . #
+            . . . . #
+            `)
+    }
+})
+let Ecran1Serpant = 0
 let IconGame = 0
 basic.showLeds(`
     . # # # .
